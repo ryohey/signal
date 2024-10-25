bfc8f70e040f03a0a98aa7c3ab6ac1724361a5a9
ryohey <info@codingcafe.jp>
2024-10-25 09:56:03 +0900

    Add MIDI Input setting
    (cherry picked from commit 51fdc4b45fddb99532a390c4589b2e581b763797)

--- app/src/components/TrackList/TrackDialog.tsx
+++ app/src/components/TrackList/TrackDialog.tsx
@@ -20,63 +20,103 @@ export interface TrackDialogProps {
   onClose: () => void
 }
 
+const ChannelSelect: FC<{
+  channel: number | undefined
+  onChange: (channel: number) => void
+}> = ({ channel, onChange }) => {
+  return (
+    <Select
+      value={channel}
+      onChange={(e) => onChange(parseInt(e.target.value as string))}
+    >
+      {range(0, 16).map((v) => (
+        <option key={v} value={v.toString()}>
+          {v + 1}
+          {v === 9 ? (
+            <>
+              {" "}
+              (<Localized name="rhythm-track" />)
+            </>
+          ) : (
+            ""
+          )}
+        </option>
+      ))}
+    </Select>
+  )
+}
+
+const MIDIInputSelect: FC<{
+  channel: number | undefined
+  onChange: (channel: number) => void
+}> = ({ channel, onChange }) => {
+  return (
+    <Select
+      value={channel}
+      onChange={(e) => onChange(parseInt(e.target.value as string))}
+    >
+      <option key={-1} value={undefined}>
+        <Localized name="midi-input-all" />
+      </option>
+      {range(0, 16).map((v) => (
+        <option key={v} value={v.toString()}>
+          {v + 1}
+        </option>
+      ))}
+    </Select>
+  )
+}
+
 export const TrackDialog: FC<TrackDialogProps> = ({ track, open, onClose }) => {
   const [name, setName] = useState(track.name)
   const [channel, setChannel] = useState(track.channel)
+  const [midiInputChannel, setMIDIInputChannel] = useState(0)
 
   useEffect(() => {
     setName(track.name)
     setChannel(track.channel)
   }, [track])
 
+  const onClickOK = () => {
+    track.channel = channel
+    track.setName(name ?? "")
+    onClose()
+  }
+
   return (
     <Dialog open={open} onOpenChange={onClose} style={{ minWidth: "20rem" }}>
       <DialogTitle>
         <Localized name="track" />: <TrackName track={track} />
       </DialogTitle>
-      <DialogContent>
+      <DialogContent
+        style={{ display: "flex", flexDirection: "column", gap: "1rem" }}
+      >
         <Label>
           <Localized name="track-name" />
+          <TextField
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value as string)}
+            style={{ width: "100%" }}
+          />
         </Label>
-        <TextField
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value as string)}
-          style={{ width: "100%", marginBottom: "1rem" }}
-        />
         <Label>
           <Localized name="channel" />
+          <ChannelSelect channel={channel} onChange={setChannel} />
         </Label>
-        <Select
-          value={channel}
-          onChange={(e) => setChannel(parseInt(e.target.value as string))}
-        >
-          {range(0, 16).map((v) => (
-            <option key={v} value={v.toString()}>
-              {v + 1}
-              {v === 9 ? (
-                <>
-                  {" "}
-                  (<Localized name="rhythm-track" />)
-                </>
-              ) : (
-                ""
-              )}
-            </option>
-          ))}
-        </Select>
+        <Label>
+          <Localized name="midi-input" />
+          <MIDIInputSelect
+            channel={midiInputChannel}
+            onChange={setMIDIInputChannel}
+          />
+        </Label>
       </DialogContent>
       <DialogActions>
         <Button autoFocus onClick={onClose}>
           <Localized name="cancel" />
         </Button>
-        <PrimaryButton
-          onClick={() => {
-            track.channel = channel
-            track.setName(name ?? "")
-            onClose()
-          }}
-        >
+        <PrimaryButton onClick={onClickOK}>
           <Localized name="ok" />
         </PrimaryButton>
       </DialogActions>
