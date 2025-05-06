f3024e73ec233149379d1d12c64ddd2a1cf9ef61
ryohey <info@codingcafe.jp>
2025-05-06 17:48:53 +0900

    Fix reactivity

--- app/src/hooks/useEventList.ts
+++ app/src/hooks/useEventList.ts
@@ -1,18 +1,24 @@
-import { useMemo } from "react"
+import { isEqual } from "lodash"
+import { toJS } from "mobx"
+import { useMobxSelector } from "./useMobxSelector"
 import { usePianoRoll } from "./usePianoRoll"
 import { useTrack } from "./useTrack"
 
 export function useEventList() {
   const { selectedTrackId, selectedNoteIds } = usePianoRoll()
   const { events: trackEvents } = useTrack(selectedTrackId)
-  const events = useMemo(() => {
-    if (selectedNoteIds.length > 0) {
-      return trackEvents.filter(
-        (event) => selectedNoteIds.indexOf(event.id) >= 0,
-      )
-    }
-    return trackEvents
-  }, [trackEvents, selectedNoteIds])
+  const events = useMobxSelector(
+    () => {
+      if (selectedNoteIds.length > 0) {
+        return trackEvents.filter(
+          (event) => selectedNoteIds.indexOf(event.id) >= 0,
+        )
+      }
+      return toJS(trackEvents)
+    },
+    [trackEvents, selectedNoteIds],
+    isEqual,
+  )
 
   return {
     events,
