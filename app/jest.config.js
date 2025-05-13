58242fcd4e34429e7c1cb82a0b358cf5e687563b
ryohey <info@codingcafe.jp>
2025-05-13 09:37:14 +0900

    Add testing-library

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  testEnvironment: "./src/test/CustomEnvironment.ts",
+  testEnvironment: "jsdom",
   preset: "ts-jest/presets/js-with-babel-esm",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  transformIgnorePatterns: ["/node_modules/(?!midifile-ts)"],
+  transformIgnorePatterns: ["/node_modules/(?!(midifile-ts|use-l10n)/)"],
 }
