a911f5fbb84a61fba12c6a66bc7e1c8b2eb2baf2
ryohey <info@codingcafe.jp>
2025-07-27 23:06:29 +0900

    chore: remove useMobxStore

--- app/src/hooks/useMobxSelector.ts
+++ app/src/hooks/useMobxSelector.ts
@@ -1,8 +1,6 @@
 import { IEqualsComparer, reaction } from "mobx"
 import { useCallback } from "react"
 import { useSyncExternalStoreWithSelector } from "use-sync-external-store/with-selector"
-import RootStore from "../stores/RootStore"
-import { useStores } from "./useStores"
 
 type Selector<T> = () => T
 
@@ -25,14 +23,6 @@ export function useMobxSelector<T>(
     useCallback((x) => x, []),
     equals,
   )
-}
-
-export function useMobxStore<T>(
-  selector: (rootStore: RootStore) => T,
-  equals?: IEqualsComparer<T>,
-): T {
-  const rootStore = useStores()
-  return useMobxSelector(() => selector(rootStore), [rootStore], equals)
 }
 
 export function useMobxGetter<T, K extends keyof T>(
