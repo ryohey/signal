a258ff42fe4b34711809c6a5dd03a002cf03690b
ryohey <info@codingcafe.jp>
2025-08-31 10:00:03 +0900

    remove static/

--- static/next.config.mjs
+++
@@ -1,8 +0,0 @@
-import { withKumaUI } from "@kuma-ui/next-plugin"
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-}
-
-export default withKumaUI(nextConfig)
--- static/src/app/privacy/LocaleDate.tsx
+++
@@ -1,5 +0,0 @@
-"use client"
-
-export const LocaleDate = ({ date }: { date: Date }) => {
-  return <>{date.toLocaleDateString()}</>
-}
