df7c165792f05522be19a621d72696b2203b499b
ryohey <info@codingcafe.jp>
2025-11-09 18:33:07 +0900

    Refactor gestures

--- app/src/components/PianoRoll/MouseHandler/usePencilGesture.ts
+++ app/src/components/PianoRoll/MouseHandler/usePencilGesture.ts
@@ -1,30 +1,20 @@
 import { MouseGesture } from "../../../gesture/MouseGesture"
-import { usePianoRoll } from "../../../hooks/usePianoRoll"
 import { useCreateNoteGesture } from "./gestures/useCreateNoteGesture"
 import { useSelectNoteGesture } from "./gestures/useSelectNoteGesture"
 
 export const usePencilGesture = (): MouseGesture => {
-  const { getLocal, getNotes } = usePianoRoll()
   const createNoteGesture = useCreateNoteGesture()
   const selectNoteGesture = useSelectNoteGesture()
 
   return {
     onMouseDown(e: MouseEvent) {
-      const local = getLocal(e)
-      const items = getNotes(local)
-
       switch (e.button) {
         case 0: {
-          if (items.length > 0) {
-            // no-op
+          if (e.shiftKey || e.metaKey) {
+            return selectNoteGesture.onMouseDown(e)
           } else {
-            if (e.shiftKey || e.metaKey) {
-              return selectNoteGesture.onMouseDown(e)
-            } else {
-              return createNoteGesture.onMouseDown(e)
-            }
+            return createNoteGesture.onMouseDown(e)
           }
-          break
         }
         default:
           return null
