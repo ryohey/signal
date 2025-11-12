f0a0a2ef9e89980eb50726fb9f8f0158d7fe4555
ryohey <info@codingcafe.jp>
2025-11-12 09:51:19 +0900

    optimize performance

--- app/src/hooks/useEventList.ts
+++ app/src/hooks/useEventList.ts
@@ -5,22 +5,22 @@ import { usePianoRoll } from "./usePianoRoll"
 import { useTrack } from "./useTrack"
 
 export function useEventList() {
-  const { selectedTrackId, selectedNoteIds } = usePianoRoll()
-  const { events: trackEvents } = useTrack(selectedTrackId)
-  const events = useMobxSelector(
-    () => {
-      if (selectedNoteIds.length > 0) {
-        return trackEvents.filter(
-          (event) => selectedNoteIds.indexOf(event.id) >= 0,
-        )
-      }
-      return toJS(trackEvents)
-    },
-    [trackEvents, selectedNoteIds],
-    isEqual,
-  )
-
   return {
-    events,
+    get events() {
+      const { selectedTrackId, selectedNoteIds } = usePianoRoll()
+      const { events: trackEvents } = useTrack(selectedTrackId)
+      return useMobxSelector(
+        () => {
+          if (selectedNoteIds.length > 0) {
+            return trackEvents.filter(
+              (event) => selectedNoteIds.indexOf(event.id) >= 0,
+            )
+          }
+          return toJS(trackEvents)
+        },
+        [trackEvents, selectedNoteIds],
+        isEqual,
+      )
+    },
   }
 }
