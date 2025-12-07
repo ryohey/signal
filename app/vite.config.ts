91d764dd0bdcfdeba330f1fea51e19b94d21b308
ryohey <info@codingcafe.jp>
2025-12-07 12:35:50 +0900

    Use vite for dev

---
+++ app/vite.config.ts
@@ -0,0 +1,44 @@
+import react from "@vitejs/plugin-react"
+import path from "path"
+import { defineConfig, loadEnv } from "vite"
+import svgr from "vite-plugin-svgr"
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+
+  return {
+    plugins: [
+      react(),
+      svgr({
+        include: "**/*.svg",
+        svgrOptions: {
+          plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx"],
+          exportType: "default",
+        },
+      }),
+    ],
+    build: {
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, "edit.html"),
+          auth: path.resolve(__dirname, "auth.html"),
+          community: path.resolve(__dirname, "community.html"),
+        },
+      },
+    },
+    publicDir: "public",
+    server: {
+      port: 3000,
+      open: "/edit",
+    },
+    resolve: {
+      alias: {
+        react: path.resolve("../node_modules/react"),
+      },
+    },
+    envDir: "..",
+    define: {
+      "process.env": env,
+    },
+  }
+})
