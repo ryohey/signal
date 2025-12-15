f74fe4c013cebe823a5f3d960919b060f0f40bb3
ryohey <info@codingcafe.jp>
2025-12-15 15:52:45 +0900

    fix type error
    (cherry picked from commit a05effbd6d34636353b42b423b4eb0842bc0362c)

--- app/src/midi/downloadSongAsMidi.ts
+++ app/src/midi/downloadSongAsMidi.ts
@@ -3,6 +3,6 @@ import { downloadBlob } from "../helpers/Downloader"
 
 export function downloadSongAsMidi(song: Song) {
   const bytes = songToMidi(song)
-  const blob = new Blob([bytes], { type: "application/octet-stream" })
+  const blob = new Blob([new Uint8Array(bytes)], { type: "application/octet-stream" })
   downloadBlob(blob, song.filepath.length > 0 ? song.filepath : "no name.mid")
 }
